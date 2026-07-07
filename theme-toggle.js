acc5c53352eefb8fc331b9dd2e1e173b4d79d924
ふぐ/Fugu <153415824+Fugu0141@users.noreply.github.com>
2026-07-07 17:06:30 +0900

    Bump loader URL for inline tab button

--- theme-toggle.js
+++ theme-toggle.js
@@ -42,7 +42,7 @@
   }
 
   function loadReleasePrepAssets() {
-    loadScriptOnce("release-prep-loader", "./release-prep-loader.js?v=20260707-12");
+    loadScriptOnce("release-prep-loader", "./release-prep-loader.js?v=20260707-13");
   }
 
   function safeGetMode() {
