d9cc776fb9f3c3113666bca111f1a30e67de1618
ふぐ/Fugu <153415824+Fugu0141@users.noreply.github.com>
2026-07-07 16:16:01 +0900

    Bump release assets after toolbar scope fix

--- release-prep-loader.js
+++ release-prep-loader.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = "20260707-10";
+  const version = "20260707-11";
 
   function loadCssOnce(id, href) {
     if (document.querySelector(`link[data-release-prep-id="${id}"]`)) return;
