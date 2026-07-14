3371e3f45276d9f4e2ca66bbfa3bf22876f692d5
ふぐ/Fugu <153415824+Fugu0141@users.noreply.github.com>
2026-07-14 22:06:54 +0900

    Bump workspace layout cache version

--- release-prep-loader.js
+++ release-prep-loader.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = "20260712-4";
+  const version = "20260712-5";
 
   function loadCssOnce(id, href) {
     if (document.querySelector(`link[data-release-prep-id="${id}"]`)) return;
