72247acdf45c83a5cdca4c0598e6c259f10ea37b
ふぐ/Fugu <153415824+Fugu0141@users.noreply.github.com>
2026-09-14 13:56:09 +0900

    feat(domain): expose annotation document API

---
+++ src/modules/annotation/index.ts
@@ -0,0 +1,8 @@
+export {
+  validateAnnotation,
+  type Annotation,
+  type AnnotationValidationError,
+  type Rect,
+  type StrokeAnnotation,
+  type TextAnnotation,
+} from './domain/annotation.ts';
