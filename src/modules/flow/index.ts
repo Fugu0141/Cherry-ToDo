4f199e4c1bc213cf718d97e4324c03624ad4ccf3
ふぐ/Fugu <153415824+Fugu0141@users.noreply.github.com>
2026-09-14 13:55:05 +0900

    feat(domain): expose Flow public API

---
+++ src/modules/flow/index.ts
@@ -0,0 +1,14 @@
+export {
+  addFlowEdge,
+  incomingStructuralEdges,
+  isDerivedBranchingGoal,
+  outgoingStructuralEdges,
+  validateFlowGraph,
+  type FlowEdge,
+  type FlowGraph,
+  type FlowInvariantError,
+  type FlowInvariantErrorCode,
+  type ReferenceFlowEdge,
+  type StructuralFlowEdge,
+  type StructuralFlowKind,
+} from './domain/flow.ts';
